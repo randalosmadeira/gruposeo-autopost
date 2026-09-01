b0bd88f13437e099a4638658eb2715597c22985e
randalosmadeira <randalos.madeira@gmail.com>
2026-09-01 04:12:22 -0300

    chore: set Zica Posts frontend version to 3.10.1

--- src/lib/plugin-version.ts
+++ src/lib/plugin-version.ts
@@ -3,7 +3,7 @@
  */
 export const PLUGIN_SOFTWARE_ID = 'zica-posts';
 export const PLUGIN_NAME = 'Zica Posts';
-export const PLUGIN_VERSION = '3.10.0';
-export const PLUGIN_LAST_UPDATE = '2026-09-01T03:00:00-03:00';
-export const PLUGIN_MINIMUM_VERSION = '3.10.0';
+export const PLUGIN_VERSION = '3.10.1';
+export const PLUGIN_LAST_UPDATE = '2026-09-01T04:15:00-03:00';
+export const PLUGIN_MINIMUM_VERSION = '3.10.1';
 export const PLUGIN_API_NAMESPACE = 'zica-posts/v1';
