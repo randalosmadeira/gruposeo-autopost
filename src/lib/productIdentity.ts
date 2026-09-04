8dc4069b4e7fae44ed343a0826b962d0cae5a474
randalosmadeira <randalos.madeira@gmail.com>
2026-09-03 23:17:46 -0300

    feat: adicionar informações institucionais

---
+++ src/lib/productIdentity.ts
@@ -0,0 +1,14 @@
+export const PRODUCT_IDENTITY = {
+  institutionalSite: {
+    label: 'www.app.zica.ia.posts.juris.com.br',
+    href: 'https://www.app.zica.ia.posts.juris.com.br',
+  },
+  contactEmail: {
+    label: 'apps.desenvolve@zicajuris.com.br',
+    href: 'mailto:apps.desenvolve@zicajuris.com.br',
+  },
+  developers: {
+    label: 'www.gruposeomkt.com.br',
+    href: 'https://www.gruposeomkt.com.br',
+  },
+} as const;
